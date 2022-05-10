5c27783bf4eee13bf790b8d8483997e3b1fb64cb
ihopeyoucanfly <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-05-10 18:33:33 +0300

    Update Card.tsx

--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -6,39 +6,10 @@ import { IProduct } from "../../../utils/interfaces";
 
 import styles from "./Card.module.scss";
 interface ICardProps {
-	product: IProduct;
+  product: IProduct;
 }
 
 const Card: FC<ICardProps> = ({ product }) => {
-<<<<<<< HEAD
-	return (
-		<Link to={`product/${product._id}`}>
-			<div className={styles.container}>
-				<div className={styles.header}>
-					<div className={styles.info}>
-						{product.hit && <p className={styles.hit}>ХИТ</p>}
-						{product.promotion && <p className={styles.promotion}>АКЦИЯ</p>}
-					</div>
-					<div className={styles.favorite}>
-						<FavoriteBorderIcon fontSize="medium" color="success" />
-					</div>
-				</div>
-				<img className={styles.img} src={product.urlImg} alt="" />
-				<p className={styles.title}>
-					{product.title.length < 49
-						? product.title
-						: product.title.slice(0, 49) + "..."}
-				</p>
-				<div className={styles.prices}>
-					<p className={styles.price}>{product.price} ₽</p>
-					{product.oldPrice && (
-						<p className={styles.oldPrice}>{product.oldPrice} ₽</p>
-					)}
-				</div>
-			</div>
-		</Link>
-	);
-=======
   return (
     <Link to={`product/${product._id}`} className={styles.wrapper}>
       <div className={styles.container}>
@@ -66,7 +37,6 @@ const Card: FC<ICardProps> = ({ product }) => {
       </div>
     </Link>
   );
->>>>>>> 4d1bafe687a2a04a70c6bda722d62c139c69750b
 };
 
 export default Card;
