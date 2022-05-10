97528bfcac95409aa20bb250c91734e13274f8bc
zamukaev <abu.safiia2016@mail.com>
2022-05-10 13:55:59 +0200

    unpdate pruduct

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,23 +1,46 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
 import styles from './product.module.scss';
 
-import large from './img/01.png';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
+import { IProduct } from '../../utils/interfaces';
+
 import small1 from './img/small1.png';
 import small2 from './img/small2.png';
 import small3 from './img/small3.png';
 
+import axios from "axios";
+
 
 
 const Product: FC = () => {
+	const { id } = useParams<string>();
+
+	const [product, setProduct] = useState<IProduct>({} as IProduct);
+
+	useEffect(() => {
+		fetchProduct()
+	}, [])
+
+	async function fetchProduct() {
+		try {
+			const response = await axios.get<IProduct>(`http://game-store12.herokuapp.com/api/products/product/${id}`)
+			setProduct(response.data)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 	return (
 		<div className={styles.product}>
 			<div className={styles.header}>
-				<span className={styles.leftArrow}></span> <h2 className={styles.title}>Геймпад PlayStation DualSense Wireless Controller для PS5 белый</h2>
+				<NavLink to="/" className={styles.leftArrow}><ChevronLeftOutlinedIcon color="disabled" fontSize="medium" /></NavLink> <h2 className={styles.title}>{product.title}</h2>
 			</div>
 			<div className={styles.content}>
 				<div className={styles.productImg}>
 					<div className={styles.large}>
-						<img src={large} alt="product" />
+						<img src={product.urlImg} alt="productImage" />
 					</div>
 					<div className={styles.small}>
 						<div><img src={small1} alt="small" /></div>
@@ -26,15 +49,16 @@ const Product: FC = () => {
 					</div>
 				</div>
 				<div className={styles.body}>
-					<div className={styles.subtitle}>для PS5, беспроводной, USB Type-C, mini-jack, виброотдача, 10 м</div>
+					<div className={styles.subtitle}>{product.desc}</div>
 					<div className={styles.price}>
-						<span>6 999 ₽ </span>
+						<span>{product.price} ₽  </span>
+						<span className="heartIcon"><FavoriteBorderOutlinedIcon /></span>
 					</div>
 				</div>
 			</div>
 			<div className={styles.desc}>
-				<div className={styles.title}>Характеристики Геймпад PlayStation DualSense Wireless Controller для PS5 белый</div>
-				<div className={styles.body}>Проводной геймпад OKLICK GP-200M является игровым манипулятором, обеспечивающим взаимодействие между пользователем и персональным компьютером. Эргономичный дизайн для удобного и простого управления, 2 режима работы: цифровой и аналоговый (светодиодная индикация режима), 8 направлений D-пад, длина кабеля - 140 см.</div>
+				<div className={styles.title}>{product.title}</div>
+				<div className={styles.body}>{product.characteristic}</div>
 			</div>
 		</div >);
 };
