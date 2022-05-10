6c565d021ad0c2fc3d825f5cdb266211668df3ab
ihopeyoucanfly <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-05-10 11:56:38 +0300

    add promotions block in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,56 +1,17 @@
-import axios from "axios";
-import React, { FC, useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import React, { FC } from "react";
 
-import Card from "../../components/ui/card/Card";
-import MyLoader from "../../components/ui/contentLoader/ContentLoader";
-
-import { IProduct } from "../../utils/interfaces";
+import Hits from "./hits/Hits";
 
 import styles from "./Home.module.scss";
 import "swiper/css";
 import "swiper/css/navigation";
+import Promotions from "./promotions/Promotions";
 
 const Home: FC = () => {
-  const [hits, setHits] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    getHits();
-  }, []);
-
-  const getHits = async () => {
-    const res = await axios.get(
-      "http://game-store12.herokuapp.com/api/products/hits"
-    );
-    setHits(res.data);
-    setLoading(false);
-  };
-
   return (
     <div className={styles.container}>
-      <p className={styles.title}>Хиты продаж</p>
-      <div className={styles.hits}>
-        {loading ? (
-          <MyLoader />
-        ) : (
-          <Swiper
-            slidesPerView={4}
-            spaceBetween={70}
-            navigation={true}
-            className="mySwiper"
-            style={{ maxWidth: 1550 }}
-            modules={[Navigation]}
-          >
-            {hits.map((product) => (
-              <SwiperSlide key={product._id}>
-                <Card product={product} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
-      </div>
+      <Hits />
+      <Promotions />
     </div>
   );
 };
---
+++ src/pages/home/promotions/Promotions.tsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { FC, useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import MyLoader from "../../../components/ui/contentLoader/ContentLoader";
+
+import { IProduct } from "../../../utils/interfaces";
+import Card from "../../../components/ui/card/Card";
+
+import styles from "../Home.module.scss";
+
+const Promotions: FC = () => {
+  const [promotions, setPromotions] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getPromotions();
+  }, []);
+
+  const getPromotions = async () => {
+    const res = await axios.get(
+      "http://game-store12.herokuapp.com/api/products/promotions"
+    );
+    setPromotions(res.data);
+    setLoading(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>Акции</p>
+      {loading ? (
+        <MyLoader />
+      ) : (
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={70}
+          navigation={true}
+          className="mySwiper"
+          style={{ maxWidth: 1550 }}
+          modules={[Navigation]}
+        >
+          {promotions.map((product) => (
+            <SwiperSlide key={product._id}>
+              <Card product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  );
+};
+
+export default Promotions;
