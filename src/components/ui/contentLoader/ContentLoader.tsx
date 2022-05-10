747d5bb92014d73da4c70b4afe7dad1ef89793ef
ihopeyoucanfly <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-05-10 11:40:15 +0300

    add content loader in home

---
+++ src/components/ui/contentLoader/ContentLoader.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const MyLoader = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "space-between",
+      marginTop: 30,
+      width: " -webkit-fill-available",
+    }}
+  >
+    <ContentLoader
+      speed={2}
+      width={272}
+      height={393}
+      viewBox="0 0 272 393"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="13" y="6" rx="0" ry="0" width="74" height="25" />
+      <rect x="101" y="6" rx="0" ry="0" width="74" height="25" />
+      <circle cx="248" cy="18" r="16" />
+      <rect x="44" y="81" rx="0" ry="0" width="191" height="155" />
+      <rect x="35" y="275" rx="0" ry="0" width="212" height="14" />
+      <rect x="33" y="362" rx="0" ry="0" width="99" height="25" />
+      <rect x="35" y="299" rx="0" ry="0" width="212" height="14" />
+      <rect x="35" y="325" rx="0" ry="0" width="212" height="14" />
+    </ContentLoader>
+    <ContentLoader
+      speed={2}
+      width={272}
+      height={393}
+      viewBox="0 0 272 393"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="13" y="6" rx="0" ry="0" width="74" height="25" />
+      <rect x="101" y="6" rx="0" ry="0" width="74" height="25" />
+      <circle cx="248" cy="18" r="16" />
+      <rect x="44" y="81" rx="0" ry="0" width="191" height="155" />
+      <rect x="35" y="275" rx="0" ry="0" width="212" height="14" />
+      <rect x="33" y="362" rx="0" ry="0" width="99" height="25" />
+      <rect x="35" y="299" rx="0" ry="0" width="212" height="14" />
+      <rect x="35" y="325" rx="0" ry="0" width="212" height="14" />
+    </ContentLoader>
+    <ContentLoader
+      speed={2}
+      width={272}
+      height={393}
+      viewBox="0 0 272 393"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="13" y="6" rx="0" ry="0" width="74" height="25" />
+      <rect x="101" y="6" rx="0" ry="0" width="74" height="25" />
+      <circle cx="248" cy="18" r="16" />
+      <rect x="44" y="81" rx="0" ry="0" width="191" height="155" />
+      <rect x="35" y="275" rx="0" ry="0" width="212" height="14" />
+      <rect x="33" y="362" rx="0" ry="0" width="99" height="25" />
+      <rect x="35" y="299" rx="0" ry="0" width="212" height="14" />
+      <rect x="35" y="325" rx="0" ry="0" width="212" height="14" />
+    </ContentLoader>
+    <ContentLoader
+      speed={2}
+      width={272}
+      height={393}
+      viewBox="0 0 272 393"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="13" y="6" rx="0" ry="0" width="74" height="25" />
+      <rect x="101" y="6" rx="0" ry="0" width="74" height="25" />
+      <circle cx="248" cy="18" r="16" />
+      <rect x="44" y="81" rx="0" ry="0" width="191" height="155" />
+      <rect x="35" y="275" rx="0" ry="0" width="212" height="14" />
+      <rect x="33" y="362" rx="0" ry="0" width="99" height="25" />
+      <rect x="35" y="299" rx="0" ry="0" width="212" height="14" />
+      <rect x="35" y="325" rx="0" ry="0" width="212" height="14" />
+    </ContentLoader>
+  </div>
+);
+
+export default MyLoader;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 
 import Card from "../../components/ui/card/Card";
+import MyLoader from "../../components/ui/contentLoader/ContentLoader";
 
 import { IProduct } from "../../utils/interfaces";
 
@@ -13,6 +14,7 @@ import "swiper/css/navigation";
 
 const Home: FC = () => {
   const [hits, setHits] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     getHits();
@@ -23,26 +25,31 @@ const Home: FC = () => {
       "http://game-store12.herokuapp.com/api/products/hits"
     );
     setHits(res.data);
+    setLoading(false);
   };
 
   return (
     <div className={styles.container}>
       <p className={styles.title}>Хиты продаж</p>
       <div className={styles.hits}>
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={70}
-          navigation={true}
-          className="mySwiper"
-          style={{ maxWidth: 1550 }}
-          modules={[Navigation]}
-        >
-          {hits.map((product) => (
-            <SwiperSlide key={product._id}>
-              <Card product={product} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {loading ? (
+          <MyLoader />
+        ) : (
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={70}
+            navigation={true}
+            className="mySwiper"
+            style={{ maxWidth: 1550 }}
+            modules={[Navigation]}
+          >
+            {hits.map((product) => (
+              <SwiperSlide key={product._id}>
+                <Card product={product} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   );
