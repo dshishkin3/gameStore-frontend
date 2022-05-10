4d1bafe687a2a04a70c6bda722d62c139c69750b
ihopeyoucanfly <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-05-10 17:14:13 +0300

    fix bag

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,72 +1,82 @@
-<<<<<<< HEAD
-import React, { FC } from "react";
-import styles from "./product.module.scss";
-=======
 import React, { FC, useEffect, useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
-import styles from './product.module.scss';
+import styles from "./product.module.scss";
 
-import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
-import { IProduct } from '../../utils/interfaces';
+import { IProduct } from "../../utils/interfaces";
 
-import small1 from './img/small1.png';
-import small2 from './img/small2.png';
-import small3 from './img/small3.png';
+import small1 from "../../assets/images/product/small1.png";
+import small2 from "../../assets/images/product/small2.png";
+import small3 from "../../assets/images/product/small3.png";
 
 import axios from "axios";
 
->>>>>>> 97528bfcac95409aa20bb250c91734e13274f8bc
-
 const Product: FC = () => {
+  const { id } = useParams<string>();
 
-	const { id } = useParams<string>();
+  const [product, setProduct] = useState<IProduct>({} as IProduct);
+  const [loading, setLoading] = useState<boolean>(true);
 
-	const [product, setProduct] = useState<IProduct>({} as IProduct);
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
-	useEffect(() => {
-		fetchProduct()
-	}, [])
-
-	async function fetchProduct() {
-		try {
-			const response = await axios.get<IProduct>(`http://game-store12.herokuapp.com/api/products/product/${id}`)
-			setProduct(response.data)
-		} catch (e) {
-			console.log(e)
-		}
-	}
-	return (
-		<div className={styles.product}>
-			<div className={styles.header}>
-				<NavLink to="/" className={styles.leftArrow}><ChevronLeftOutlinedIcon color="disabled" fontSize="medium" /></NavLink> <h2 className={styles.title}>{product.title}</h2>
-			</div>
-			<div className={styles.content}>
-				<div className={styles.productImg}>
-					<div className={styles.large}>
-						<img src={product.urlImg} alt="productImage" />
-					</div>
-					<div className={styles.small}>
-						<div><img src={small1} alt="small" /></div>
-						<div><img src={small2} alt="small" /></div>
-						<div><img src={small3} alt="small" /></div>
-					</div>
-				</div>
-				<div className={styles.body}>
-					<div className={styles.subtitle}>{product.desc}</div>
-					<div className={styles.price}>
-						<span>{product.price} ₽  </span>
-						<span className="heartIcon"><FavoriteBorderOutlinedIcon /></span>
-					</div>
-				</div>
-			</div>
-			<div className={styles.desc}>
-				<div className={styles.title}>{product.title}</div>
-				<div className={styles.body}>{product.characteristic}</div>
-			</div>
-		</div >);
->>>>>>> 97528bfcac95409aa20bb250c91734e13274f8bc
+  async function fetchProduct() {
+    try {
+      const response = await axios.get<IProduct>(
+        `http://game-store12.herokuapp.com/api/products/product/${id}`
+      );
+      setProduct(response.data);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return loading ? (
+    <h1>loading</h1>
+  ) : (
+    <div className={styles.product}>
+      <div className={styles.header}>
+        <NavLink to="/" className={styles.leftArrow}>
+          <ChevronLeftOutlinedIcon color="disabled" fontSize="medium" />
+        </NavLink>{" "}
+        <h2 className={styles.title}>{product.title}</h2>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.productImg}>
+          <div className={styles.large}>
+            <img src={product.urlImages[0]} alt="productImage" />
+          </div>
+          <div className={styles.small}>
+            <div>
+              <img src={small1} alt="small" />
+            </div>
+            <div>
+              <img src={small2} alt="small" />
+            </div>
+            <div>
+              <img src={small3} alt="small" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.body}>
+          <div className={styles.subtitle}>{product.desc}</div>
+          <div className={styles.price}>
+            <span>{product.price} ₽ </span>
+            <span className="heartIcon">
+              <FavoriteBorderOutlinedIcon />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.desc}>
+        <div className={styles.title}>{product.title}</div>
+        <div className={styles.body}>{product.characteristic}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
