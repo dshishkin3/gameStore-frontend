c5a5ab6dcf3c1b9e3f00576983e80471fca31a6e
ihopeyoucanfly <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-06-02 21:53:43 +0300

    add admin panel guide

--- src/admin-panel/pages/Home/Home.tsx
+++ src/admin-panel/pages/Home/Home.tsx
@@ -1,13 +1,32 @@
+import axios from "axios";
 import React, { FC } from "react";
 import Wrapper from "../../components/ui/wrapper/Wrapper";
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
 
 const AdminHome: FC = () => {
-	return (
-		<Wrapper title="">
-			<div className={styles.container}>Home admin </div>
-		</Wrapper>
-	);
+  return (
+    <Wrapper title="">
+      <p className={styles.title}>Небольшая инструкция по админ панели</p>
+
+      <ul className={styles.block}>
+        <p className={styles.blockTitle}>Авторизация:</p>
+        <li className={styles.list}>game-store.ru/admin/auth</li>
+        <li className={styles.list}>
+          В админ панели стараться не обновлять страницу, т.к придется
+          переавторизовываться
+        </li>
+      </ul>
+      <ul className={styles.block}>
+        <p className={styles.blockTitle}>Редактирование товаров:</p>
+        <li className={styles.list}>
+          Обязательно проверить, чтобы все поля были заполнены{" "}
+        </li>
+        <li className={styles.list}>
+          После изменений не забыть нажать “сохранить изменения”
+        </li>
+      </ul>
+    </Wrapper>
+  );
 };
 
 export default AdminHome;
